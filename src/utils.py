2f5ccaa7e8441bb8e534cbdcef7937f20a7dcf3c
Mohnish Shandilya <mohnishshandilya.3000@gmail.com>
2026-04-12 03:15:18 +0530

    Hyperparameter Tuning and Cross Validation

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,8 @@ import dill
 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import KFold
 
 def save_object(file_path, obj):
     try:
@@ -19,13 +21,23 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
 
+            kf = KFold(n_splits=3)
+
+            clf = GridSearchCV(model, param, cv=kf)
+            clf.fit(X_train, y_train)
+            
+            model.set_params(**clf.best_params_)
+
+            #Training the model after the hyperparameter tuning
             model.fit(X_train, y_train)
+
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
             train_model_score = r2_score(y_train, y_train_pred)
@@ -33,7 +45,7 @@ def evaluate_model(X_train, y_train, X_test, y_test, models):
 
             report[(list(models.keys()))[i]] = test_model_score
 
-        return report
+        return report 
 
     except Exception as e:
         raise CustomException(e, sys)
